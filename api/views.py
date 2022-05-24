06216d4d4bd9ef46d7cfa5815635e63d88bfbe13
Leonel Higuera <lhiguer1@asu.edu>
2022-05-24 09:08:02 -0700

    add caching to api requests

--- api/views.py
+++ api/views.py
@@ -1,18 +1,36 @@
 from django.http import JsonResponse
 from django.core.handlers.wsgi import WSGIRequest
 from db.models import Number
+from django.core.cache import cache
 
 # Create your views here.
 def get_numbers(request: WSGIRequest, **kwargs):
     """Return a JsonResponse object of the dates and numbers of the specified dates."""
+    year:int  = kwargs.get('year')
+    month:int = kwargs.get('month')
+    day:int   = kwargs.get('day')
 
-    numbers = Number.objects.filter(date__year=kwargs['year'])
+    # cache_key is isoformat string; YYYY-MM-DD or YYYY-MM or YYYY
+    cache_key = f'{year:04d}'
+    if month:
+        cache_key += f'-{month:02d}'
+    if day:
+        cache_key += f'-{day:02d}'
 
-    if month := kwargs.get('month'):
-        numbers = numbers.filter(date__month=month)
-    if day := kwargs.get('day'):
-        numbers = numbers.filter(date__day=day)
+    if cached_data := cache.get(cache_key):
+        json_numbers = cached_data
 
-    json_numbers = {number.date.isoformat(): number.number for number in numbers}
+    else:
+        numbers = Number.objects.filter(date__year=year)
+
+        if month:
+            numbers = numbers.filter(date__month=month)
+        if day:
+            numbers = numbers.filter(date__day=day)
+
+        json_numbers = {number.date.isoformat(): number.number for number in numbers}
+
+        cache.set(cache_key, json_numbers, None)
+
     return JsonResponse(json_numbers)
                         
