c5fe38bbff101014387efe5727e326054f34fcc5
Leonel Higuera <lhiguer1@asu.edu>
2022-06-09 22:00:06 -0700

    change primary id from `date` to `id`

--- db/models.py
+++ db/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Number(models.Model):
-    date = models.DateField(primary_key=True)
+    date = models.DateField()
     number = models.PositiveSmallIntegerField()
     url = models.URLField()
 
