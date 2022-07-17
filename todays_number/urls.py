2e6772ab09b3fa0a7a4381890aeb3f2904018e41
Leonel Higuera <lhiguer1@asu.edu>
2022-07-16 21:32:04 -0700

    add openapi schema

--- todays_number/urls.py
+++ todays_number/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
+from rest_framework import permissions
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+
+    path('openapi/', get_schema_view(
+            title="Today's Number API",
+            description="DAVID LYNCH THEATER Presents: Today's Number Is...",
+            version="1.0.0",
+            permission_classes=[permissions.AllowAny]
+        ), name='openapi-schema'),
 ]
