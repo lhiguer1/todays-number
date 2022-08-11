874369b3ed193c9afabde6b8c3cdd8543a1f2dc7
Leonel Higuera <lhiguer1@asu.edu>
2022-08-11 14:23:41 -0700

    fix .env

--- todays_number/settings.py
+++ todays_number/settings.py
@@ -27,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+SECRET_KEY = config['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG') == 'True'
+DEBUG = config['DJANGO_DEBUG'] == 'True'
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
