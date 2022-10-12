58745821240dff9a01856470abcdab228972eefc
Leonel Higuera <lhiguer1@asu.edu>
2022-10-12 15:58:00 -0700

    re-implement python-dotenv

--- todays_number/settings.py
+++ todays_number/settings.py
@@ -11,8 +11,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import dj_database_url
 import os
+from dotenv import load_dotenv
+import dj_database_url
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
