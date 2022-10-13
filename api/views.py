a596e6aeee3b49c623d7e5d854706b610bf96191
Leonel Higuera <lhiguer1@asu.edu>
2022-10-12 22:48:49 -0700

    use default list view

--- api/views.py
+++ api/views.py
@@ -32,17 +32,6 @@ class NumberListView(generics.ListAPIView):
 
         return queryset
 
-    def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-
-        serializer = self.get_serializer(queryset, many=True)
-        return Response({'numbers': serializer.data})
-
 class NumberCreateView(generics.CreateAPIView):
     queryset = Number.objects.all()
     serializer_class = NumberSerializer
