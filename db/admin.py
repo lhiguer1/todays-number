322ea3ca52a2244f9f73a989782aef3a12333b2f
Leonel Higuera <lhiguer1@asu.edu>
2022-11-15 23:06:43 -0700

    custom ModelAdmin class

--- db/admin.py
+++ db/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Number
 
 # Register your models here.
-admin.site.register(Number)
+class NumberAdmin(admin.ModelAdmin):
+    list_display = ['date', 'number', 'url']
+    list_filter = ['date']
+
+admin.site.register(Number, NumberAdmin)
