422f640556b9b3a9bc99cd69dc94a56bb71c7c44
Leonel Higuera <lhiguer1@asu.edu>
2022-11-30 19:03:40 -0700

    add singleton link

--- lynchnumbers/serializers.py
+++ lynchnumbers/serializers.py
@@ -4,6 +4,7 @@ from rest_framework import serializers
 class NumberSerializer(serializers.ModelSerializer):
     date = serializers.DateField(format='iso-8601', input_formats=['iso-8601'])
     number = serializers.IntegerField(min_value=1, max_value=10)
+    singleton = serializers.HyperlinkedIdentityField(view_name='number-detail', lookup_field='date')
     url = serializers.URLField(allow_blank=False)
     transcript = serializers.CharField(allow_blank=False, trim_whitespace=True)
 
@@ -12,6 +13,7 @@ class NumberSerializer(serializers.ModelSerializer):
         fields = (
             'date',
             'number',
+            'singleton',
             'url',
             'transcript',
         )
