a37a6cc85d208c8d8776b7f853b7f87999f79e1d
Leonel Higuera <lhiguer1@asu.edu>
2022-12-24 20:25:20 -0700

    update `NumberAdmin` to disallow date edits from admin page

--- lynchnumbers/admin.py
+++ lynchnumbers/admin.py
@@ -3,7 +3,14 @@ from .models import Number
 
 # Register your models here.
 class NumberAdmin(admin.ModelAdmin):
+    fields = [
+        'date',
+        'number',
+        'url',
+        'transcript',
+    ]
     list_display = ['date', 'number', 'url']
     list_filter = ['date']
+    readonly_fields = ['date']
 
 admin.site.register(Number, NumberAdmin)
