8749f480d21d98c3cd23a61d020ac463c68f703d
Leonel Higuera <lhiguer1@asu.edu>
2022-12-24 20:45:10 -0700

    cleanup: remove unused views

--- lynchnumbers/urls.py
+++ lynchnumbers/urls.py
@@ -4,17 +4,6 @@ from .viewset import NumberViewset
 from .views import StatisticsView
 
 
-# register_converter(FourDigitYearConverter, 'yyyy')
-# register_converter(TwoDigitMonthConverter, 'mm')
-# register_converter(TwoDigitDayConverter, 'dd')
-
-# urlpatterns = [
-#     path('', NumberListCreateAPIView.as_view(), name='number-list'),
-#     path('<yyyy:year>/', NumberListAPIView.as_view(), name='number-year-list'),
-#     path('<yyyy:year>/<mm:month>/', NumberListAPIView.as_view(), name='number-month-list'),
-#     path('<yyyy:year>/<mm:month>/<dd:day>/', NumberRetrieveUpdateDestroyAPIView.as_view(), name='number-detail'),
-# ]
-
 router = TodaysNumberRouter()
 router.register(r'numbers', NumberViewset, basename='number')
 
--- lynchnumbers/views.py
+++ lynchnumbers/views.py
@@ -1,57 +1,14 @@
 import datetime
 import statistics
 import collections
-from django.db.models import Q, QuerySet
-from django.shortcuts import get_object_or_404
+from django.db.models import QuerySet
 from rest_framework import (
-    exceptions,
-    generics,
-    mixins,
-    serializers,
     response,
     reverse,
     views,
 )
-from .mixins import BaseNumberMixin, BaseAuthenticationPermission
 from .models import Number
 
-
-class NumberListAPIView(BaseNumberMixin, BaseAuthenticationPermission,generics.ListAPIView):
-    def get_queryset(self):
-        """
-        Return queryset filtered by specified date. Return all if none is provided.
-        """
-        qs:QuerySet = super().get_queryset()
-        lookup = Q()
-
-        for field in self.kwargs:
-            lookup &= Q(**{f'date__{field}': self.kwargs[field]})
-
-        qs = qs.filter(lookup)
-
-        return qs
-
-class NumberListCreateAPIView(mixins.CreateModelMixin, NumberListAPIView):
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-class NumberRetrieveUpdateDestroyAPIView(BaseNumberMixin, BaseAuthenticationPermission, generics.RetrieveUpdateDestroyAPIView):
-    date = serializers.DateField(read_only=True, format='iso-8601', input_formats=['iso-8601'])
-
-    def get_object(self):
-        queryset = self.filter_queryset(self.get_queryset())
-
-        try:
-            date = datetime.date.fromisoformat('{year:04}-{month:02}-{day:02}'.format_map(self.kwargs))
-        except ValueError as e:
-            raise exceptions.NotFound(e)
-
-        obj = get_object_or_404(queryset, date=date)
-
-        # May raise a permission denied
-        self.check_object_permissions(self.request, obj)
-
-        return obj
 
 class StatisticsView(views.APIView):
     def get(self, request, *args, **kwargs):
