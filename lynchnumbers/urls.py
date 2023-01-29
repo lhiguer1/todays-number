9c1c795b31a40edcf2e8bc8e1868169fd9d730f8
Leonel Higuera <lhiguer1@asu.edu>
2023-01-29 02:02:30 -0700

    create frontend

--- lynchnumbers/urls.py
+++ lynchnumbers/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework import routers
-from .views import StatisticsView, NumberViewset
+from .views import StatisticsView, NumberViewset, HomeView, AboutView, DocumentationView
 
 
 router = routers.DefaultRouter()
@@ -8,6 +8,10 @@ router.include_root_view = False
 
 router.register(r'numbers', NumberViewset, basename='number')
 
-urlpatterns = router.urls + [
-    path('statistics/', StatisticsView.as_view(), name='statistics'),
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path('about/', AboutView.as_view(), name='about'),
+    path('docs/', DocumentationView.as_view(), name='docs'),
+    path('api/', include(router.urls)),
+    path('api/statistics/', StatisticsView.as_view(), name='statistics'),
 ]
